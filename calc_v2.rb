d94e5079cf7b49f87ac3c6458e0a5bdbc42951ac
Vanessa Vengco <vengco@gmail.com>
2013-07-15 01:17:19 -0700

    Change tabs to two-space indents per Ruby style conventions

--- calc_v2.rb
+++ calc_v2.rb
@@ -5,28 +5,28 @@ puts "Welcome to your calculator!"
 puts "Press enter to continue."
 
 while gets.chomp != 'q' do
-	msg = "Please provide an arithmetic operator."
-	puts msg
-	op = gets.chomp
+  msg = "Please provide an arithmetic operator."
+  puts msg
+  op = gets.chomp
 
 	while !['+', '-', '*', '/'].include?(op) do
-		puts msg
-		op = gets.chomp
-	end
+    puts msg
+    op = gets.chomp
+  end
 
-	# ask for two numbers
-	puts "Please give me a number."
-	num1 = gets.chomp
-	puts "Please give me a second number."
-	num2 = gets.chomp
+  # ask for two numbers
+  puts "Please give me a number."
+  num1 = gets.chomp
+  puts "Please give me a second number."
+  num2 = gets.chomp
 
-	num1 = num1.to_f.to_s
-	num2 = num2.to_f.to_s
+  num1 = num1.to_f.to_s
+  num2 = num2.to_f.to_s
 
-	# do calculation and print result
+  # do calculation and print result
 
-	puts "The result is #{eval(num1 + op + num2)}." 	#Using string interpolation
-	puts "Press enter to calculate more numbers, or 'q' to quit."
+  puts "The result is #{eval(num1 + op + num2)}." 	#Using string interpolation
+  puts "Press enter to calculate more numbers, or 'q' to quit."
 end
 
 # continue until q
